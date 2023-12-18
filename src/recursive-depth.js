e1889f499e4a9d5ce5363d5f5e397b2faa8e8570
Yar Night <yaroslav239@gmail.com>
2023-12-18 19:24:09 +0300

    feat: add task15, task 16, task17

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth( arr ) {
+    if (!Array.isArray(arr))  return 0;
+    let recurse = 0;
+    for (let item of arr)
+    recurse = Math.max(recurse, this.calculateDepth(item));
+    return recurse + 1;
   }
 }
 
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink( value ) {
+		if (value !== undefined) {
+      this.chain.push(String(value));
+    } else {
+      this.chain.push('( )');
+    }
+		return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink( position ) {
+		if (Number.isInteger(position) && (this.chain[position - 1] !== undefined)) {
+			this.chain.splice(position - 1, 1);
+		} else {
+			this.chain = [];
+			throw new Error("You can't remove incorrect link!");
+		}
+		return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+		this.chain.reverse();
+		return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+		let result = '';
+		if (this.chain.length === 0) return result;
+    result = `( ${this.chain[0]} )`;
+		for (let i = 1; i < this.chain.length; i++) {
+			result = result + `~~( ${this.chain[i]} )`;
+		}
+		this.chain = [];
+		return result;
   }
 };
 
