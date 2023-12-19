cbb77feeb69d446a5cf24e701057d014c55abf53
Yar Night <yaroslav239@gmail.com>
2023-12-19 20:17:12 +0300

    feat: add task18, task19, task20, task21

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform( arr ) {
+  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+
+  let result = [];
+
+  let options = {
+    "--discard-next" : (i) => {
+      if (arr[i + 1] > 0) {
+        arr.splice(i + 1, 1);
+      }
+    },
+    "--discard-prev" : (i) => {
+      if (arr[i - 1] > 0) {
+        result.splice(i - 1, 1);
+      }
+    },
+    "--double-next" : (i) => {
+      if (arr[i + 1] > 0) {
+        result.push(arr[i + 1]);
+      }
+    },
+    "--double-prev" : (i) => {
+      if (result[result.length - 1] > 0 && arr[i - 1] !== '--discard-next') {
+        result.push(result[result.length - 1]);
+      }
+    },
+  };
+  arr.forEach((item, index) => {
+    options[item] ?  options[item](index) : result.push(item);
+  })
+
+  return result;
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,81 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(item = true) {
+    this.item = item;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key, isDecrypt = false) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+    const shift = {
+      A: 0,
+      B: 1,
+      C: 2,
+      D: 3,
+      E: 4,
+      F: 5,
+      G: 6,
+      H: 7,
+      I: 8,
+      J: 9,
+      K: 10,
+      L: 11,
+      M: 12,
+      N: 13,
+      O: 14,
+      P: 15,
+      Q: 16,
+      R: 17,
+      S: 18,
+      T: 19,
+      U: 20,
+      V: 21,
+      W: 22,
+      X: 23,
+      Y: 24,
+      Z: 25,
+    };
+
+    let result = "";
+    let count = 0;
+    let upperKey = key.toUpperCase();
+    let char = null;
+    
+    while (upperKey.length < message.length) {
+      upperKey += upperKey;
+    }
+    upperKey = upperKey.slice(0, message.length);
+
+
+    const arr = [...message.toUpperCase()];
+    
+    arr.forEach((element) => {
+      if (element.charCodeAt() >= 65 && element.charCodeAt() <= 90) {
+        if (!isDecrypt) {
+          char = element.charCodeAt() + shift[upperKey[count++]];
+          if (char > 90) {
+            char = char - 90 + 64;
+          }
+        } else {
+          char = element.charCodeAt() - shift[upperKey[count++]];
+          if (char < 65) {
+            char = 90 - 64 + char;
+          }
+        }
+
+        result += String.fromCharCode(char);
+      } else {
+        result += element;
+      }
+    });
+    return this.item ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(message, key) {
+    return this.encrypt(message, key, true);
   }
 }
 
